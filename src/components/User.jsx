6499c2810d6469be66ca12ecf82eefb80c092e00
Maria Eduarda Melo de Sousa <mariiaeduarda1215@gmail.com>
2025-11-18 12:40:56 +0000

    Desafio

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import { useContext } from "react";
-import { CartContext } from "../context/CartContext";
-import styles from "./User.module.css";
-
-export function User() {
-  const { session, handleSignOut } = useContext(CartContext);
-  return (
-    <div>
-      {session ? (
-        <div className={styles.container}>
-          {session.user.user_metadata.admin ? (
-            <h1>Admin Account</h1>
-          ) : (
-            <h1>User Account</h1>
-          )}
-          <div className={styles.userInfo}>
-            <p>
-              <strong>Username:</strong> {session.user.user_metadata.username}
-            </p>
-            <p>
-              <strong>Email:</strong> {session.user.email}
-            </p>
-            <p>
-              <strong>ID:</strong> {session.user.id}
-            </p>
-          </div>
-          <button className={styles.button} onClick={handleSignOut}>
-            SIGN OUT
-          </button>
-        </div>
-      ) : (
-        <div className={styles.container}>
-          <h1>User not signed in!</h1>
-        </div>
-      )}
-    </div>
-  );
-}
-=======
 import { useContext, useState } from "react";
 import { SessionContext } from "../context/SessionContext";
 import { CartContext } from "../context/CartContext";
@@ -255,4 +215,3 @@ export function User() {
     </div>
   );
 }
->>>>>>> 352ae62 (Desafio)
